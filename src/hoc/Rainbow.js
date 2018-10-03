98a83ad33ad2644f4f2fc54c604e7387052184f4
RiCK BROWN <ricky.brown.00@gmail.com>
2018-10-04 00:05:52 +0100

    Added redux

--- src/hoc/Rainbow.js
+++ src/hoc/Rainbow.js
@@ -4,7 +4,6 @@ const Rainbow = (WrappedComponent) => {
   const colors = ['red', 'pink', 'orange', 'blue', 'green', 'yellow']
   const randomColor = colors[Math.floor(Math.random()*5)]
   const className = randomColor + '-text'
-  console.log(className);
 
   return (props) => {
     return(
